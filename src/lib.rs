dd13547aef6de19418e23877535fc00c38b6cc01
lyranowl <nikita.malina23@gmail.com>
2024-08-04 23:49:21 +0900

    removed `Uri` (I will put it in a separate crate), updated `bytes` in dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,9 @@ pub mod method;
 pub mod request;
 pub mod response;
 pub mod status;
-pub mod uri;
 pub mod version;
 
 pub use self::{
-    method::Method, request::Request, response::Response, status::StatusCode, uri::Uri,
+    method::Method, request::Request, response::Response, status::StatusCode,
     version::Version,
 };
